ccd4e32f270bdb73e32abed2d50de367211c3824
seruhio5 <sergiomolero5@gmail.com>
2016-12-31 11:35:11 +0100

    Creacion de DTO y assembler de Respuesta

--- MouseRidersWeb/Models/Assembler/RespuestaAssembler.cs
+++ MouseRidersWeb/Models/Assembler/RespuestaAssembler.cs
@@ -12,16 +12,20 @@ namespace MRWeb.Assembler
 {
     public class RespuestaAssembler
     {
-        //public RespuestaDTO Convert(RespuestaEN resp)
-        //{
-        //    RespuestaDTO respDTO = null;
-        //    if (resp != null)
-        //    {
-        //        respDTO = new RespuestaDTO();
-        //        respDTO.Id = resp.Id;
-        //    }
-        //    return respDTO;
-        //}
+        public RespuestaDTO Convert(RespuestaEN resp)
+        {
+            RespuestaDTO respDTO = null;
+            if (resp != null)
+            {
+                respDTO = new RespuestaDTO();
+                respDTO.Id = resp.Id;
+                respDTO.Respuesta = respDTO.Respuesta;
+                respDTO.Tipo = resp.Tipo;
+                respDTO.Contador = resp.Contador;
+                respDTO.Frecuencia = resp.Frecuencia;
+            }
+            return respDTO;
+        }
         
     }
 }
---
+++ MouseRidersWeb/Models/DTO/RespuestaDTO.cs
@@ -0,0 +1,52 @@
+﻿using MRModel.Enumerated;
+using MRWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+// Definición clase RespuestaDTO
+namespace MRWeb.DTO
+{
+    public class RespuestaDTO
+    {
+        //Atributo Id
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+
+        /**
+         *	Atributo Respuesta
+         */
+        [Display(Prompt = "Respuesta", Description = "Respuesta", Name = "Respuesta:")]
+        [Required(ErrorMessage = "Debe tener ua Respuesta")]
+        [StringLength(maximumLength: Globals.TITULO_MAX_LENGTH, ErrorMessage = "La Respuesta no puede tener más de {0} caracteres")]
+        public string Respuesta { get; set; }
+
+
+        //Atributo Tipo
+        [Display(Prompt = "Tipo", Description = "Tipo del mensaje", Name = "Tipo:")]
+        [Required(ErrorMessage = "Debe existir un tipo")]
+        //[StringLength(maximumLength: Globals.TIPO_MAX_LENGTH, ErrorMessage = "El tipo del mensaje no puede tener más de {0} caracteres")]
+        public T_PreguntaEnum Tipo { get; set; }
+
+
+        /**
+        *	Atributo contador
+        */
+        [Display(Prompt = "Contador", Description = "Contador", Name = "Contador:")]
+        //[StringLength(maximumLength: Globals.CONTENIDO_MAX_LENGTH, ErrorMessage = "El contenido descargable no puede tener más de {0} caracteres")]
+        public int Contador { get; set; }
+
+        /**
+       *	Atributo Frecuencia
+       */
+        [Display(Prompt = "Frecuencia", Description = "Frecuencia", Name = "Frecuencia:")]
+        //[StringLength(maximumLength: Globals.CONTENIDO_MAX_LENGTH, ErrorMessage = "El contenido descargable no puede tener más de {0} caracteres")]
+        public float Frecuencia { get; set; }
+
+        //Relaciones
+
+    }
+}
