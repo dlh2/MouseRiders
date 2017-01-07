e9928a3bcda380967c697c2f3827cafce9f5ae7b
Luiso1996 <gambaruga1@hotmail.com>
2017-01-07 18:37:25 +0100

    <3

---
+++ MouseRidersGenNHibernate/CEN/MouseRiders/HiloCEN_actualizarNumCom.cs
@@ -0,0 +1,33 @@
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Criterion;
+using NHibernate.Exceptions;
+using MouseRidersGenNHibernate.Exceptions;
+using MouseRidersGenNHibernate.EN.MouseRiders;
+using MouseRidersGenNHibernate.CAD.MouseRiders;
+
+
+/*PROTECTED REGION ID(usingMouseRidersGenNHibernate.CEN.MouseRiders_Hilo_actualizarNumCom) ENABLED START*/
+//  references to other libraries
+/*PROTECTED REGION END*/
+
+namespace MouseRidersGenNHibernate.CEN.MouseRiders
+{
+public partial class HiloCEN
+{
+public int ActualizarNumCom (int p_oid, int sumar_o_restar)
+{
+        /*PROTECTED REGION ID(MouseRidersGenNHibernate.CEN.MouseRiders_Hilo_actualizarNumCom) ENABLED START*/
+
+        // Write here your custom code...
+
+        throw new NotImplementedException ("Method ActualizarNumCom() not yet implemented.");
+
+        /*PROTECTED REGION END*/
+}
+}
+}
