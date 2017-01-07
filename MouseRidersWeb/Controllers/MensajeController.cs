3905ef6a50c9dd8bf93ca3e05a8cfbe7ce5c1835
Luiso1996 <gambaruga1@hotmail.com>
2017-01-08 00:47:54 +0100

    <3

--- MouseRidersWeb/Controllers/MensajeController.cs
+++ MouseRidersWeb/Controllers/MensajeController.cs
@@ -40,6 +40,110 @@ namespace MouseRidersWeb.Controllers
        
         }
 
+        // GET: /Mensaje/Create
+
+        public ActionResult Sugerir()
+        {
+            MensajeEN men = new MensajeEN();
+            MensajeDTO result = new MensajeAssembler().Convert(men);
+            return View(result);
+        }
+
+        //
+        // POST: /Mensaje/Create
+
+        [HttpPost]
+        public ActionResult Sugerir(MensajeEN men)
+        {
+
+            MensajeCAD cCAD = new MensajeCAD();
+            MensajeCEN cen = new MensajeCEN(cCAD);
+            IList<int> aux = new List<int>();
+            aux.Add(32768);
+            int id = cen.CrearMensaje(men.Asunto, men.Texto, men.Adjunto, men.Tipo, 32768, aux);
+
+            return RedirectToAction("Details", new { id = id });
+
+        }
+
+        // GET: /Mensaje/Create
+
+        public ActionResult Redactor()
+        {
+            MensajeEN men = new MensajeEN();
+            MensajeDTO result = new MensajeAssembler().Convert(men);
+            return View(result);
+        }
+
+        //
+        // POST: /Mensaje/Create
+
+        [HttpPost]
+        public ActionResult Redactor(MensajeEN men)
+        {
+
+            MensajeCAD cCAD = new MensajeCAD();
+            MensajeCEN cen = new MensajeCEN(cCAD);
+            IList<int> aux = new List<int>();
+            aux.Add(32768);
+            int id = cen.CrearMensaje(men.Asunto, men.Texto, men.Adjunto, men.Tipo, 32768, aux);
+
+            return RedirectToAction("Details", new { id = id });
+
+        }
+
+        // GET: /Mensaje/Create
+
+        public ActionResult Administrador()
+        {
+            MensajeEN men = new MensajeEN();
+            MensajeDTO result = new MensajeAssembler().Convert(men);
+            return View(result);
+        }
+
+        //
+        // POST: /Mensaje/Create
+
+        [HttpPost]
+        public ActionResult Administrador(MensajeEN men)
+        {
+
+            MensajeCAD cCAD = new MensajeCAD();
+            MensajeCEN cen = new MensajeCEN(cCAD);
+            IList<int> aux = new List<int>();
+            aux.Add(32768);
+            int id = cen.CrearMensaje(men.Asunto, men.Texto, men.Adjunto, men.Tipo, 32768, aux);
+
+            return RedirectToAction("Details", new { id = id });
+
+        }
+
+        // GET: /Mensaje/Create
+
+        public ActionResult Solicitar()
+        {
+            MensajeEN men = new MensajeEN();
+            MensajeDTO result = new MensajeAssembler().Convert(men);
+            return View(result);
+        }
+
+        //
+        // POST: /Mensaje/Create
+
+        [HttpPost]
+        public ActionResult Solicitar(MensajeEN men)
+        {
+
+            MensajeCAD cCAD = new MensajeCAD();
+            MensajeCEN cen = new MensajeCEN(cCAD);
+            IList<int> aux = new List<int>();
+            aux.Add(32768);
+            int id = cen.CrearMensaje(men.Asunto, men.Texto, men.Adjunto, men.Tipo, 32768, aux);
+
+            return RedirectToAction("Details", new { id = id });
+
+        }
+
         // GET: /Mensaje/Details/5
         public ActionResult Details(int id)
         {
