005a71e674e39c2e9e7fb3772d85b7fd826e0c58
seruhio5 <sergiomolero5@gmail.com>
2017-01-08 18:38:44 +0100

    Manual de usuario

--- MouseRidersWeb/Controllers/UsuarioController.cs
+++ MouseRidersWeb/Controllers/UsuarioController.cs
@@ -11,11 +11,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebMatrix.WebData;
+using System.IO;
 
 namespace MouseRidersWeb.Controllers
 {
     public class UsuarioController : BasicController
     {
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/Images"), fileName);
+                    file.SaveAs(path);
+                }
+                ViewBag.Message = "Upload successful";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ViewBag.Message = "Upload failed";
+                return RedirectToAction("Uploads");
+            }
+        }
+
         //
         // GET: /Usuario/
 
